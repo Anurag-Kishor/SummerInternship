596463e872dc78dca9a2d2b06543c40fec48a017
Anurag <anuragkishor2011@gmail.com>
2021-07-03 23:35:12 +0530

    Created Setup and Registration Page UI

---
+++ Xamarin/AndroidAppUITask/AndroidAppUITask/AndroidAppUITask/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+[assembly: ExportFont("Poppins-Regular.ttf", Alias = "Poppins")]
+[assembly: ExportFont("Poppins-Bold.ttf", Alias = "PoppinsBold")]
+[assembly: ExportFont("Poppins-SemiBold.ttf", Alias = "PoppinsSemiBold")]
+
+namespace AndroidAppUITask
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Xamarin/AndroidAppUITask/AndroidAppUITask/AndroidAppUITask/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AndroidAppUITask
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Agent_Clicked(object sender, EventArgs e)
+        {
+        }
+
+        public void Controller_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegistrationPage());
+
+        }
+    }
+}
